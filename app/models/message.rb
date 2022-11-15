6cc56ca0420e3cb69a32d134954801b9cb9bff2a
Amador Fernandez <amador.fernandez@factorial.co>
2022-11-15 09:13:54 +0000

    Websocket implementation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :body, presence:true
+  #Scope permit us to filter by only recovering the last 20 messages in the example. 
+  scope :custom_display, -> { order(:created_at).last(20) }
+
 end
